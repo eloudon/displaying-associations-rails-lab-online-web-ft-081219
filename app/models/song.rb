89c16522306bc3ff8642397aa2db8f262fe5a20f
Eric Loudon <loudon.eric@gmail.com>
2019-10-28 11:12:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,13 +9,3 @@ class Song < ActiveRecord::Base
     self.artist_name + " - " + self.title
   end
 end
-
-
-  def artist_name
-  self.artist.name
-end
-
-def index_display_name
-  self.artist_name + " - " + self.title
-end
-end
